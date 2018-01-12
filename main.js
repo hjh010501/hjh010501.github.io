63d92a1a19604efa970db41e7d645a84877c5f45
KID <hjh010501@naver.com>
2018-01-13 02:20:02 +0900

    test

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-/**
- * Created by KID on 2017. 8. 3..
- */
+// /**
+//  * Created by KID on 2017. 8. 3..
+//  */
 
-window.onload = load;
+// window.onload = load;
 
-function load() {
-    var random = Math.floor(Math.random() * 2);
+// function load() {
+//     var random = Math.floor(Math.random() * 2);
 
-    if(random == 0) {
-        document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio_b.png)";
-        document.getElementsByTagName('body')[0].style.backgroundColor = "#000000";
-    } else {
-        document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio.png)";
-    }
+//     if(random == 0) {
+//         document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio_b.png)";
+//         document.getElementsByTagName('body')[0].style.backgroundColor = "#000000";
+//     } else {
+//         document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio.png)";
+//     }
 
-}
+// }
