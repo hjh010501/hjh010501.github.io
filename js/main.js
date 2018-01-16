5e6f7699faaa262bc130dd2d5d40973242e17768
KID <hjh010501@naver.com>
2018-01-17 00:21:20 +0900

    Clearing file path, add scrolling animation

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// /**
+//  * Created by KID on 2017. 8. 3..
+//  */
+
+// window.onload = load;
+
+// function load() {
+//     var random = Math.floor(Math.random() * 2);
+
+//     if(random == 0) {
+//         document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio_b.png)";
+//         document.getElementsByTagName('body')[0].style.backgroundColor = "#000000";
+//     } else {
+//         document.getElementsByClassName('kidevelop')[0].style.backgroundImage = "url(kidevelopio.png)";
+//     }
+
+// }
