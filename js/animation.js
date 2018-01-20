992ec1078b3f0cedf7c11ca6e849602e864a0854
KID <hjh010501@naver.com>
2018-01-21 03:09:41 +0900

    Clearing source of mouse cursor animation

--- js/animation.js
+++ js/animation.js
@@ -27,13 +27,16 @@ $( window ).scroll(function() {
 });
 
 $( window ).mousemove(function(event) {
+    
     scrollvalue = $window.scrollTop();
     $( ".mousecursor" ).css({
         "left": (event.pageX - 25) + "px",
-        "top": (event.pageY - 25 - scrollvalue) + "px"
+        "top": (event.pageY - 25 - scrollvalue) + "px",
+        "opacity": 1
       });
     $( ".real_mousecursor" ).css({
         "left": (event.pageX - 5) + "px",
-        "top": (event.pageY - 5 - scrollvalue ) + "px"
+        "top": (event.pageY - 5 - scrollvalue ) + "px",
+        "opacity": 1
     });
 });
